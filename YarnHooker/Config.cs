90890817818603bb6e58177cf5b9a43b68ecb715
IruzzArcana <iruzzgames@gmail.com>
2026-02-16 22:40:32 +0100

    Add project files.

---
+++ YarnHooker/Config.cs
@@ -0,0 +1,33 @@
+﻿using MelonLoader;
+
+namespace YarnHooker
+{
+    internal class Config
+    {
+        private static MelonPreferences_Category category;
+        private static MelonPreferences_Entry<string> filepath;
+        private const string m_cfg = "UserData/yarn.cfg";
+        private const string m_deffilepath = "yarnproj.yarnc";
+
+        public static string FilePath => filepath.Value;
+
+        public static void Init()
+        {
+            category = MelonPreferences.CreateCategory("YarnHooker");
+            category.SetFilePath(m_cfg, false);
+
+            category.LoadFromFile();
+
+            if (category.HasEntry("filepath"))
+            {
+                filepath = category.GetEntry<string>("filepath");
+            }
+            else
+            {
+                filepath = category.CreateEntry("filepath", m_deffilepath);
+                category.SaveToFile();
+            }
+        }
+
+    }
+}
---
+++ YarnHooker/Core.cs
@@ -0,0 +1,49 @@
+﻿using HarmonyLib;
+using Il2CppSystem;
+using MelonLoader;
+using UnityEngine.InputSystem.Utilities;
+
+[assembly: MelonInfo(typeof(YarnHooker.Core), "YarnHooker", "1.0.0", "IRUZZ", null)]
+[assembly: MelonGame("Nino", "project_nonmp")]
+
+namespace YarnHooker
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+            Config.Init();
+        }
+    }
+
+    [HarmonyPatch(typeof(Il2CppYarn.Dialogue), "SetNode", typeof(string))]
+    public class SetNodePatch
+    {
+        public static void Prefix(Il2CppYarn.Dialogue __instance, string __0)
+        {
+            if (!Il2CppSystem.IO.File.Exists(Config.FilePath))
+            {
+                MelonLogger.Error($"{Config.FilePath} not found");
+                return;
+            }
+
+            Il2CppYarn.Program prog_bak = __instance.Program;
+            try
+            {
+                __instance.LoadProgram(Config.FilePath);
+                Il2CppSystem.Collections.Generic.IEnumerable<string> NodeNames = __instance.NodeNames;
+                if (NodeNames.IndexOf(__0) < 0)
+                {
+                    MelonLogger.Warning($"{__0} not found");
+                    __instance.SetProgram(prog_bak);
+                }
+            }
+            catch (System.Exception e)
+            {
+                MelonLogger.Error(e.Message);
+                __instance.SetProgram(prog_bak);
+            }
+        }
+    }
+}
